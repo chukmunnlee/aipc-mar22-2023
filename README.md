aa481cd33b1356820c710241a86a5e7397f7ef4b
Chuk <chukmunnlee@gmail.com>
2023-03-22 12:27:29 +0800

    AM demos

---
+++ README.md
@@ -0,0 +1,1 @@
+# Demos for AIPC class Mar 22 2023
---
+++ day01/resources.tf
@@ -0,0 +1,43 @@
+
+# pull the image
+# docker pull <image>
+resource "docker_image" "dov-bear" {
+    name = var.image_name
+}
+
+# run the container
+# docker run -d -p 8080:3000 <image>
+resource "docker_container" "dov-bear" {
+    count = var.instance_count
+    name = "my-dov-bear-${count.index}"
+    image = docker_image.dov-bear.image_id
+    ports {
+        internal = 3000
+        external = 3000 + count.index
+    }
+    env = [
+        "INSTANCE_NAME=my-dov-bear"
+    ]
+}
+
+output "dov_ports" {
+    value = docker_container.dov-bear[*].ports[0].external
+    # value = [ 
+    #     for d in docker_container.dov-bear: [ 
+    #         for p in d.ports: p[*].external
+    #     ] 
+    # ]
+}
+
+# data digitalocean_ssh_key "apic" {
+#     name = "aipc"
+# }
+
+# output aipc_fingerprint {
+#     description = "AIPC fingerprint"
+#     value = data.digitalocean_ssh_key.apic.fingerprint
+# }
+
+# output aipc_public_key {
+#     value = data.digitalocean_ssh_key.apic.public_key
+# }
